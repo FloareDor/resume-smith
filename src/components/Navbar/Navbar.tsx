5206926d7d16f5c7decd76852445773bb73dd715
Ra <ravicity999@gmail.com>
2024-05-28 00:16:47 +0530

    feat: added navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,23 @@
 const Navbar = () => {
 	return (
-		<h1>Navbar</h1>
+		<div className="flex flex-row items-center justify-between gap-20 sm:px-24 sm:py-6">
+			<span className="text-black">ResumeSmith</span>
+			<div className="flex flex-row items-center gap-8">
+				<span className="text-black text-sm">Features</span>
+				<span className="text-black text-sm">Templates</span>
+				<span className="text-black text-sm">Testimonials</span>
+			</div>
+
+			<div className="flex flex-row items-center gap-8">
+				<span className="text-black text-sm">Login</span>
+				<span className="text-black text-sm">Sign up</span>
+				<span className="bg-white hover:bg-gray-200 text-black py-2 px-4 rounded outline outline-2 outline-black">
+					Format Resume
+				</span>
+				
+			</div>
+
+		</div>
 	)
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div className="bg-white w-full h-full">
+    <div className="bg-white w-full h-full min-h-screen">
       <Navbar />
     </div>
   )
