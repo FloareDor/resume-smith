7ee97abebb93ea5ea9befe5ad1d6df6c1c11c8eb
Ra <ravicity999@gmail.com>
2024-05-29 18:22:59 +0530

    feat: added homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Home from "@/components/Content/Home";
 
-const Home = () => {
+const home = () => {
   return (
     <div className="bg-white w-full h-full min-h-screen">
       <Navbar />
+      <Home />
       <Footer />
     </div>
   )
 };
 
-export default Home;
+export default home;
